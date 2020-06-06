bb01ab5a23373b056b9a5f6dbf0e84d89de47adb
Will C <2willaddiction@gmail.com>
2020-06-06 19:55:19 -0400

    add csvtojson

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 'use-strict';
 const yelp = require('yelp-fusion');
-const client = yelp.client('Iunr7_96olhfyFnglcIhdcZ8XnNr2Czo7M8g8Sw6fbF1XXFLcOA28aFlya4kQDyFKvywg2CmwR1hiwDtUn32Yifl2UCtKhGPLrIrY4s7WtKDWXAaBV1m0K47rNzbXnYx')
+const client = yelp.client('Iunr7_96olhfyFnglcIhdcZ8XnNr2Czo7M8g8Sw6fbF1XXFLcOA28aFlya4kQDyFKvywg2CmwR1hiwDtUn32Yifl2UCtKhGPLrIrY4s7WtKDWXAaBV1m0K47rNzbXnYx');
 const C2J = require('csvtojson');
 
-C2J().fromFile()
-    .then(businesses=>{
+C2J().fromFile('./src/csv/rbbDineBlackRaw.csv').then(
+    (businesses) => {
         console.log(businesses);
-    }).catch(e=>{
+    }).catch(e => {
         console.log(e);
     });
 
