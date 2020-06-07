63315a6141d5d15a5b7ec59a5a024bf498de9df5
Will C <2willaddiction@gmail.com>
2020-06-07 03:08:29 -0400

    redo

--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
 'use-strict';
 const yelp = require('yelp-fusion');
 const client = yelp.client('Iunr7_96olhfyFnglcIhdcZ8XnNr2Czo7M8g8Sw6fbF1XXFLcOA28aFlya4kQDyFKvywg2CmwR1hiwDtUn32Yifl2UCtKhGPLrIrY4s7WtKDWXAaBV1m0K47rNzbXnYx');
-const C2J = require('csvtojson');
+const csvFilePath = 'src/csv/rbbDineBlackRaw.csv';
+const csv = require('csvtojson');
 
-C2J().fromFile('./src/csv/rbbDineBlackRaw.csv').then(
-    (businesses) => {
-        console.log(businesses);
-    }).catch(e => {
-        console.log(e);
+csv()
+.fromFile(csvFilePath)
+.then((businesses)=>{
+    businesses.forEach(element => {
+
+        let busName = element.businessName;
+        let phone = element.phone;
+        let city = element.city;
+        let state = element.state;
+        let zip_code = ""
+
+        if (phone != "" && element.zipCode == ""){
+
+            console.log(phone);
+
+        }    
     });
+    
+});
 
-client.phoneSearch({
-    phone: '+17709742323'
-}).then(response =>{
-    console.log(response.jsonBody.businesses[0].name);
-}).catch(e=>{
-    console.log(e);
-})
+function yelpMatch(pnum, name,state, city){
+    client.businessMatch({
+
+        name: name,
+        phone: pnum,
+        city: city,
+        state: state,
+        match_threshold: "strict"
+
+    }).then(response =>{
+
+        zipCode = response.jsonBody.businesses[0].zip_code;
+
+    }).catch(e=>{
+
+        console.log(e);
+
+    });
+};
