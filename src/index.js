a43d10104bd44750c1e060bee969869df3ef79b2
Will C <2willaddiction@gmail.com>
2020-06-07 13:13:08 -0400

    add yelpmatch call in main function

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ const yelp = require('yelp-fusion');
 const client = yelp.client('Iunr7_96olhfyFnglcIhdcZ8XnNr2Czo7M8g8Sw6fbF1XXFLcOA28aFlya4kQDyFKvywg2CmwR1hiwDtUn32Yifl2UCtKhGPLrIrY4s7WtKDWXAaBV1m0K47rNzbXnYx');
 const csvFilePath = 'src/csv/rbbDineBlackRaw.csv';
 const csv = require('csvtojson');
+const fs = require('fs');
+let json;
+
 
 csv()
 .fromFile(csvFilePath)
 .then((businesses)=>{
-    businesses.forEach(element => {
+    json = businesses
+    json.forEach(element => {
 
         let busName = element.businessName;
         let phone = element.phone;
@@ -16,8 +20,7 @@ csv()
         let zip_code = ""
 
         if (phone != "" && element.zipCode == ""){
-
-            console.log(phone);
+            yelpMatch(phone,busName,state,city)
 
         }    
     });
@@ -34,8 +37,13 @@ function yelpMatch(pnum, name,state, city){
         match_threshold: "strict"
 
     }).then(response =>{
-
-        zipCode = response.jsonBody.businesses[0].zip_code;
+        try {
+            fs.appendFileSync('src/csv/dataUpdates.json',JSON.stringify(response.jsonBody));
+        }catch(e) {
+            console.error(e);
+        }
+        
+        /**return zipCode = response.jsonBody.businesses[0].zip_code;**/
 
     }).catch(e=>{
 
