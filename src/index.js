522ef7272eef5d88d9a3acfec84f2da6c4447806
Dale Race <race.dale@gmail.com>
2020-06-07 20:25:49 -0500

    Update gitignore and auto format index.js

--- src/index.js
+++ src/index.js
@@ -1,56 +1,53 @@
-'use-strict';
-const yelp = require('yelp-fusion');
-const client = yelp.client('Iunr7_96olhfyFnglcIhdcZ8XnNr2Czo7M8g8Sw6fbF1XXFLcOA28aFlya4kQDyFKvywg2CmwR1hiwDtUn32Yifl2UCtKhGPLrIrY4s7WtKDWXAaBV1m0K47rNzbXnYx');
-const csvFilePath = 'src/csv/rbbDineBlackRaw.csv';
-const csv = require('csvtojson');
-const fs = require('fs');
-let json;
-
+"use-strict"
+const yelp = require("yelp-fusion")
+const client = yelp.client(
+  "Iunr7_96olhfyFnglcIhdcZ8XnNr2Czo7M8g8Sw6fbF1XXFLcOA28aFlya4kQDyFKvywg2CmwR1hiwDtUn32Yifl2UCtKhGPLrIrY4s7WtKDWXAaBV1m0K47rNzbXnYx"
+)
+const csvFilePath = "src/csv/rbbDineBlackRaw.csv"
+const csv = require("csvtojson")
+const fs = require("fs")
+const mapLimit = require("async/mapLimit")
+let json
 
 csv()
-.fromFile(csvFilePath)
-.then((businesses)=>{
+  .fromFile(csvFilePath)
+  .then((businesses) => {
     json = businesses
-    json.forEach(element => {
+    json.forEach((element) => {
+      let busName = element.businessName
+      let phone = element.phone
+      /**let city = json[0].city;**/
+      let loc = element.city + ", " + element.state
+      let zip_code = ""
 
-        let busName = element.businessName;
-        let phone = element.phone;
-        /**let city = json[0].city;**/
-        let loc= element.city+", "+element.state;
-        let zip_code = ""
+      if (phone != "" && element.zipCode == "") {
+        mapLimit(list, rateLimit, yelpMatch)
+        yelpMatch(phone, busName, loc)
+      }
+    })
+  })
 
-        if (phone != "" && element.zipCode == ""){
-            yelpMatch(phone,busName,loc)
+function yelpMatch(pnum, name, loc) {
+  client
+    .phoneSearch({
+      /**term: name,**/
+      phone: pnum,
+      /**location: loc**/
+    })
+    .then((response) => {
+      try {
+        fs.readFile("src/csv/dataUpdates.json", (e, data) => {
+          json = JSON.parse(data)
+          json.push(response.jsonBody.businesses[0])
+          fs.writeFileSync("src/csv/dataUpdates.json", JSON.stringify(json))
+        })
+      } catch (e) {
+        console.error(e)
+      }
 
-        }    
-    });
-    
-});
-
-function yelpMatch(pnum, name,loc){
-    client.phoneSearch({
-
-        /**term: name,**/
-        phone: pnum
-        /**location: loc**/
-
-    }).then(response =>{
-        try {
-            fs.readFile('src/csv/dataUpdates.json', (e,data)=>{
-                json = JSON.parse(data);
-                json.push(response.jsonBody.businesses[0]);
-                fs.writeFileSync('src/csv/dataUpdates.json',JSON.stringify(json));
-            });
-            
-        }catch(e) {
-            console.error(e);
-        }
-        
-        /**return zipCode = response.jsonBody.businesses[0].zip_code;**/
-
-    }).catch(e=>{
-
-        console.log(e);
-
-    });
-};
+      /**return zipCode = response.jsonBody.businesses[0].zip_code;**/
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+}
