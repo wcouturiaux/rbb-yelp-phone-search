4fe6154e1906bb9cb3aa8e863a20a67abed86b9d
Will C <2willaddiction@gmail.com>
2020-06-17 23:56:57 -0400

    Add command line arguments

--- src/yelpPhoneSearch.js
+++ src/yelpPhoneSearch.js
@@ -8,13 +8,21 @@ const fs = require("fs")
 const mapLimit = require("async/mapLimit")
 const YELP_API_KEY = process.env.YELP_API_KEY
 const client = yelp.client(YELP_API_KEY)
+const yargs = require('yargs')
 
+const options = yargs
+  .usage('Usage: -c path/to/file.csv -j path/to/file.json')
+  .option('c', {alias: "csv", describe: "The path to the csv file with phone numbers",
+    type: "string", demandOption: true})
+  .option('j', {alias: "json", describe: "Path to json file to write yelp data",
+    type: "string", demandOption: true})
+    .argv
 /**Set File Paths */
 const csvFilePath = "/Users/WillC/rbb-yelp-phone-search/src/csv/rbbAtlanta.csv"
 const jsonFilePath = "/Users/WillC/rbb-yelp-phone-search/src/csv/test.json"
 
 csv()
-  .fromFile(csvFilePath)
+  .fromFile(options.csv)
   .then((businesses) => {
     filteredBus = businesses
       .map((element) => {
@@ -39,11 +47,11 @@ function yelpMatch(business,done) {
     client.phoneSearch({
       phone: business.phone,
     }).then(response => {
-      fs.readFile(jsonFilePath, (e,data) => {
+      fs.readFile(options.json, (e,data) => {
         json = JSON.parse(data)
         if (response.jsonBody.businesses.length !=0) {
           json.push(response.jsonBody.businesses[0])
-        fs.writeFileSync(jsonFilePath, JSON.stringify(json))
+        fs.writeFileSync(options.json, JSON.stringify(json))
         }
         console.log('resolving')
         resolve()
